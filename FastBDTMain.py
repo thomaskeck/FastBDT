4992a25bd2a6368b3bab17060975efb1da01c721
Thomas Keck <thomas@thinkpad>
2016-01-09 18:54:56 +0100

    Added clean python bindings, so the user does not have to interact with ctypes directly

--- FastBDTMain.py
+++ FastBDTMain.py
@@ -1,36 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
-
-import ctypes
-import ctypes.util
-c_double_p = ctypes.POINTER(ctypes.c_double)
-c_uint_p = ctypes.POINTER(ctypes.c_uint)
-
-FastBDT_library_path = ctypes.util.find_library('FastBDT_CInterface')
-FastBDT = ctypes.cdll.LoadLibrary(FastBDT_library_path)
-
-FastBDT.Create.restype = ctypes.c_void_p
-FastBDT.Delete.argtypes = [ctypes.c_void_p]
-FastBDT.Load.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
-FastBDT.Train.argtypes = [ctypes.c_void_p, c_double_p, c_uint_p, ctypes.c_uint, ctypes.c_uint]
-FastBDT.Analyse.argtypes = [ctypes.c_void_p, c_double_p]
-FastBDT.Analyse.restype = ctypes.c_double
-FastBDT.SetRandRatio.argtypes = [ctypes.c_void_p, ctypes.c_double]
-FastBDT.SetShrinkage.argtypes = [ctypes.c_void_p, ctypes.c_double]
-FastBDT.SetNTrees.argtypes = [ctypes.c_void_p, ctypes.c_uint]
-FastBDT.SetNBinningLevels.argtypes = [ctypes.c_void_p, ctypes.c_uint]
-FastBDT.SetNLayersPerTree.argtypes = [ctypes.c_void_p, ctypes.c_uint]
-
+sys.path.append('./python')
 
 import numpy as np
 
-def XArray(a):
-    return numpy.ascontiguousarray(a.astype(np.float64)).ctypes.data_as(c_double_p)
-
-def YArray(a):
-    return numpy.ascontiguousarray(a.astype(np.uint32)).ctypes.data_as(c_uint_p)
-
+import FastBDT
 
 def readDataFile(datafile):
     data = np.loadtxt(datafile, skiprows=1, dtype=np.float64)
@@ -47,45 +22,17 @@ def train():
     datafile = sys.argv[2]
     weightfile = sys.argv[3]
     
-    forest = FastBDT.Create()
-
-    FastBDT.SetNBinningLevels(forest, 4)
-    if len(sys.argv) > 4:
-        FastBDT.SetNBinningLevels(forest, int(sys.argv[4]))
-
-    FastBDT.SetNTrees(forest, 100)
-    if len(sys.argv) > 5:
-        FastBDT.SetNTrees(forest, int(sys.argv[5]))
-
-    FastBDT.SetNLayersPerTree(forest, 3)
-    if len(sys.argv) > 6:
-        FastBDT.SetNLayersPerTree(forest, int(sys.argv[6]))
-
-    FastBDT.SetShrinkage(forest, 0.1)
-    if len(sys.argv) > 7:
-        FastBDT.SetShrinkage(forest, float(sys.argv[7]))
-
-    FastBDT.SetRandRatio(forest, 0.5)
-    if len(sys.argv) > 8:
-        FastBDT.SetRandRatio(forest, float(sys.argv[8]))
-
+    forest = FastBDT.Classifier(*sys.argv[4:])
     X, y = readDataFile(datafile);
 
-    numberOfEvents, numberOfFeatures = X.shape
-    
-    FastBDT.Train(forest, X.ctypes.data_as(c_double_p), y.ctypes.data_as(c_uint_p), numberOfEvents, numberOfFeatures)
-    FastBDT.Save(forest, bytes(weightfile, 'utf-8'))
+    forest.fit(X, y)
+    forest.save(weightfile)
     analyse(forest, X, y);
-    FastBDT.Delete(forest)
 
 
 def analyse(forest, X, y):
     N = len(y)
-
-    p = np.zeros(N)
-    for i, row in enumerate(X):
-        p[i] = FastBDT.Analyse(forest, row.ctypes.data_as(c_double_p))
-    
+    p = forest.predict(X)
     print("Fraction of correctly categorised samples {:.4f}".format( np.sum((p > 0.5) & (y == 1) | (p <= 0.5) & (y != 1)) / N ))
     print("Signal Efficiency {:.4f}".format( np.sum((p > 0.5) & (y == 1)) / np.sum(y == 1) ))
     print("Background Efficiency {:.4f}".format( np.sum((p <= 0.5) & (y != 1)) / np.sum(y != 1) ))
@@ -102,10 +49,9 @@ def apply():
 
     X, y = readDataFile(datafile)
 
-    forest = FastBDT.Create()
-    FastBDT.Load(forest, bytes(weightfile, 'utf-8'))
+    forest = FastBDT.Classifier()
+    forest.load(weightfile)
     analyse(forest, X, y);
-    FastBDT.Delete(forest)
     return 0
 
 
@@ -119,11 +65,12 @@ def output():
 
     X, y = readDataFile(datafile)
 
-    forest = FastBDT.Create()
-    FastBDT.Load(forest, bytes(weightfile, 'utf-8'))
-    for i, row in enumerate(X):
-        print(int(y[i] == 1), FastBDT.Analyse(forest, row.ctypes.data_as(c_double_p)))
-    FastBDT.Delete(forest)
+    forest = FastBDT.Classifier()
+    forest.load(weightfile)
+    
+    p = forest.predict(X)
+    for i, p in enumerate(p):
+        print(int(y[i] == 1), p)
     return 0
 
 
---
+++ python/FastBDT.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+import ctypes
+import ctypes.util
+c_double_p = ctypes.POINTER(ctypes.c_double)
+c_uint_p = ctypes.POINTER(ctypes.c_uint)
+
+FastBDT_library_path = ctypes.util.find_library('FastBDT_CInterface')
+FastBDT_library = ctypes.cdll.LoadLibrary(FastBDT_library_path)
+
+FastBDT_library.Create.restype = ctypes.c_void_p
+FastBDT_library.Delete.argtypes = [ctypes.c_void_p]
+FastBDT_library.Load.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+FastBDT_library.Train.argtypes = [ctypes.c_void_p, c_double_p, c_uint_p, ctypes.c_uint, ctypes.c_uint]
+FastBDT_library.Analyse.argtypes = [ctypes.c_void_p, c_double_p]
+FastBDT_library.Analyse.restype = ctypes.c_double
+FastBDT_library.SetRandRatio.argtypes = [ctypes.c_void_p, ctypes.c_double]
+FastBDT_library.SetShrinkage.argtypes = [ctypes.c_void_p, ctypes.c_double]
+FastBDT_library.SetNTrees.argtypes = [ctypes.c_void_p, ctypes.c_uint]
+FastBDT_library.SetNBinningLevels.argtypes = [ctypes.c_void_p, ctypes.c_uint]
+FastBDT_library.SetNLayersPerTree.argtypes = [ctypes.c_void_p, ctypes.c_uint]
+
+
+def PrintVersion():
+    FastBDT_library.PrintVersion()
+
+
+class Classifier(object):
+    def __init__(self, nBinningLevels=4, nTrees=100, nLayersPerTree=3, shrinkage=0.1, randRatio=0.5):
+        self.forest = FastBDT_library.Create()
+
+        FastBDT_library.SetNBinningLevels(self.forest, int(nBinningLevels))
+        FastBDT_library.SetNTrees(self.forest, int(nTrees))
+        FastBDT_library.SetNLayersPerTree(self.forest, int(nLayersPerTree))
+        FastBDT_library.SetShrinkage(self.forest, float(shrinkage))
+        FastBDT_library.SetRandRatio(self.forest, float(randRatio))
+
+    def fit(self, X, y):
+        X_temp = np.require(X, dtype=np.float64, requirements=['A', 'W', 'C', 'O'])
+        y_temp = np.require(y, dtype=np.uint32, requirements=['A', 'W', 'C', 'O'])
+        numberOfEvents, numberOfFeatures = X_temp.shape
+        FastBDT_library.Train(self.forest, X_temp.ctypes.data_as(c_double_p), y_temp.ctypes.data_as(c_uint_p), int(numberOfEvents), int(numberOfFeatures))
+        return self
+
+    def predict(self, X):
+        X_temp = np.require(X, dtype=np.float64, requirements=['A', 'W', 'C', 'O'])
+        N = len(X)
+        p = np.zeros(N)
+        for i, row in enumerate(X_temp):
+            p[i] = FastBDT_library.Analyse(self.forest, row.ctypes.data_as(c_double_p))
+        return p
+
+    def save(self, weightfile):
+        FastBDT_library.Save(self.forest, bytes(weightfile, 'utf-8'))
+
+    def load(self, weightfile):
+        FastBDT_library.Load(self.forest, bytes(weightfile, 'utf-8'))
+
+    def __del__(self):
+        FastBDT_library.Delete(self.forest)
