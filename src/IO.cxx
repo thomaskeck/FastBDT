8830d58c31cf54c4b0341b543d2f7cf7d816d5f1
Thomas Keck <thomas@thinkpad>
2016-03-17 14:04:49 +0100

    Fixed NaN and Infinity Problems if one uses application-phase without preprocessing

--- src/IO.cxx
+++ src/IO.cxx
@@ -29,5 +29,45 @@ namespace FastBDT {
      stream << std::endl;
      return stream;
   }
+  
+  /**
+   * This function reads a Cut from an std::istream
+   * @param stream an std::istream reference
+   * @param cut containing read data
+   */
+  template<>
+  std::istream& operator>>(std::istream& stream, Cut<float> &cut) {
+     stream >> cut.feature;
+
+     // Unfortunately we have to use stod here to correctly parse NaN and Infinity
+     // because usualy istream::operator>> doesn't do this!
+     std::string index_string;
+     stream >> index_string;
+     cut.index = std::stof(index_string);
+     //stream >> cut.index;
+     stream >> cut.valid;
+     stream >> cut.gain;
+     return stream;
+  }
+  
+  /**
+   * This function reads a Cut from an std::istream
+   * @param stream an std::istream reference
+   * @param cut containing read data
+   */
+  template<>
+  std::istream& operator>>(std::istream& stream, Cut<double> &cut) {
+     stream >> cut.feature;
+
+     // Unfortunately we have to use stod here to correctly parse NaN and Infinity
+     // because usualy istream::operator>> doesn't do this!
+     std::string index_string;
+     stream >> index_string;
+     cut.index = std::stod(index_string);
+     //stream >> cut.index;
+     stream >> cut.valid;
+     stream >> cut.gain;
+     return stream;
+  }
 
 }
