b5580a7fd7f416117c321283388078b2d05a4f3d
Thomas Keck <thomas.keck2@kit.edu>
2016-08-15 11:15:12 +0200

    Improved gauss example

--- examples/gauss.py
+++ examples/gauss.py
@@ -4,14 +4,14 @@ import FastBDT
 
 import numpy as np
 import sklearn.metrics
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#import matplotlib as mpl
 
 if __name__ == '__main__':
 
     mean = [0.0, 1.0, 2.0, 3.0]
     cov = [[1.0, 0.6, 0.4, 0.2],
-           [0.0, 1.0, 0.5, 0.0],
+           [0.0, 1.0, 0.99, 0.0],
            [0.0, 0.0, 1.0, 0.0],
            [0.0, 0.0, 0.0, 1.0]]
     for i in range(len(mean)):
@@ -21,18 +21,30 @@ if __name__ == '__main__':
     data = np.random.multivariate_normal(mean, cov, N_train + N_test)
     X_train, y_train = data[:N_train, 1:], data[:N_train, 0] > 0 
     X_test, y_test = data[N_train:, 1:], data[N_train:, 0] > 0 
+
     clf = FastBDT.Classifier().fit(X=X_train, y=y_train)
     p = clf.predict(X_test)
-
     print(clf.variableRanking())
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_test, p)
-    auc = sklearn.metrics.auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=4, label='ROC Integral = {:.3}'.format(auc))
-    plt.xlabel('False Positive Rate (Type I Error)')
-    plt.ylabel('True Positive Rate (Efficiency)')
-    plt.xlim((0.0,1.0))
-    plt.xlim((0.0,1.0))
-    plt.legend(loc='lower right')
+    global_auc = sklearn.metrics.roc_auc_score(y_test, p)
+    print("Global AUC", global_auc)
+
+    for i in range(3):
+        print("Without feature", i)
+        X_train_temp = np.delete(X_train, i, axis=1)
+        X_test_temp = np.delete(X_test, i, axis=1)
+        clf = FastBDT.Classifier().fit(X=X_train_temp, y=y_train)
+        p = clf.predict(X_test_temp)
+        print(clf.variableRanking())
+        auc = sklearn.metrics.roc_auc_score(y_test, p)
+        print("AUC", auc, (global_auc - auc)/global_auc)
+
+
+    #plt.plot(fpr, tpr, lw=4, label='ROC Integral = {:.3}'.format(auc))
+    #plt.xlabel('False Positive Rate (Type I Error)')
+    #plt.ylabel('True Positive Rate (Efficiency)')
+    #plt.xlim((0.0,1.0))
+    #plt.xlim((0.0,1.0))
+    #plt.legend(loc='lower right')
     #plt.show()
     #figure = plt.gcf() # get current figure
     #figure.set_size_inches(24, 16)
