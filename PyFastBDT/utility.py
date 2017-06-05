d2aa311d963d4ded35a444895f314a622e9173dc
Thomas Keck <t.keck@online.de>
2017-06-06 00:27:14 +0200

    Fixed flatness_score

--- PyFastBDT/utility.py
+++ PyFastBDT/utility.py
@@ -1,20 +1,38 @@
 import numpy as np
 
 
-def flatness(feature, probability, target):
+def flatness(probability, feature, target, weights=None, classes=[0, 1]):
+    """ 
+    Calculates the flatness of a feature under cuts on a signal probability
+    @param f the feature values
+    @param p the probability values
+    @param w optional weights
+    @return the mean standard deviation between the local and global cut selection efficiency
+    """
     quantiles = list(range(101))
     flatness_score = 0
-    for m in [target == 1, target == 0]: 
-        binning_feature = np.unique(np.percentile(feature[m], q=quantiles))
-        binning_probability = np.unique(np.percentile(probability[m], q=quantiles))
+    for m in [target == c for c in classes]:
+        p = probability[m]
+        f = feature[m]
+        if weights is None:
+            w = None
+        else:
+            w = weights[m]
 
-        hist_n, _ = np.histogramdd(np.c_[probability[m], feature[m]],
-                                   bins=[binning_probability, binning_feature])
+        binning_feature = np.unique(np.percentile(f, q=quantiles))
+        binning_probability = np.unique(np.percentile(p, q=quantiles))
+        hist_n, _ = np.histogramdd(np.c_[p, f], 
+                                   bins=[binning_probability, binning_feature],
+                                   weights=w)
         hist_inc = hist_n.sum(axis=1)
-        hist_n /= hist_n.sum(axis=0)
         hist_inc /= hist_inc.sum(axis=0)
-        flatness_score += (hist_n.T - hist_inc).std()
+        hist_n /= hist_n.sum(axis=0)
+        hist_n = hist_n.cumsum(axis=0)
+        hist_inc = hist_inc.cumsum(axis=0)
+        diff = (hist_n.T - hist_inc)**2
+        flatness_score += diff.sum() / (100*99)
     return flatness_score
+
 
 
 def auc_roc(probability, target):
